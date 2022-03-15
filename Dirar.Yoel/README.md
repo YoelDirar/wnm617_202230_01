24138c654158a549b81165667c90c7b0e75cc9a4
Yoel Dirar <yoeldirar@Yoels-MBP.attlocal.net>
2022-03-14 23:22:38 -0700

    Changed project to Dog tags, and orginzed folders

--- Dirar.Yoel/README.md
+++ Dirar.Yoel/README.md
@@ -4,12 +4,11 @@
 
 ## Imoportant (cool) Links
 
--http://yoeldirar.com
 
--http://yoeldirar.com/aau/wnm617/Dirar.yoel
 
--http://yoeldirar.com/aau/wnm617/Dirar.Yoel/Profile/index.html
+-http://yoeldirar.com/aau/wnm617/Dirar.Yoel/index.html
 
--http://yoeldirar.com/aau/wnm617/Dirar.Yoel/zen_garden/index.html
 
--http://yoeldirar.com/aau/wnm617/Dirar.Yoel/Dirar_m03_Explorations.pdf
+
+
+
--- Dirar.Yoel/js/app.js
+++ Dirar.Yoel/js/app.js
@@ -1,1 +1,37 @@
-app.js
+$(() => {
+	checkUserId();
+	//EVENT DELEGATION//
+	$(document)
+
+	//Form submissions 
+	.on ("submit", "#signin-form", function(e){
+		e.preventDefault();
+		console.log(e)
+		checkLoginForm();
+	})
+
+	// CLICKS
+	.on("click", ".js-logout", function(){
+		sessionStorage.removeItem("userId");
+		checkUserId();
+	})
+
+   // ACTIVATE TOOLS
+   .on("click", "[data-activate]", function() {
+      let target = $(this).data("activate");
+      $(target).addClass("active")
+   })
+   .on("click", "[data-deactivate]", function() {
+      let target = $(this).data("deactivate");
+      $(target).removeClass("active")
+   })
+   .on("click", "[data-toggle]", function() {
+      let target = $(this).data("toggle");
+      $(target).toggleClass("active")
+   })
+   .on("click", "[data-activateone]", function() {
+      let target = $(this).data("activate");
+      $(target).addClass("active")
+         .siblings().removeClass("active")
+   })
+});
